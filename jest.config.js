da06f1735e6a365d65484209b3d24b7fbd96332d
wuruoyun <wuruoyun@gmail.com>
2019-06-28 13:40:38 +0800

    Update the dependencies to match Vue CLI 3.8.4.

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
   transform: {
     '^.+\\.vue$': 'vue-jest',
     '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
-    '^.+\\.jsx?$': 'babel-jest'
+    '^.+\\.jsx?$': '<rootDir>/node_modules/babel-jest'
   },
   transformIgnorePatterns: [
     '/node_modules/'
@@ -22,5 +22,9 @@ module.exports = {
   testMatch: [
     '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
   ],
-  testURL: 'http://localhost/'
+  testURL: 'http://localhost/',
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname'
+  ]
 }
